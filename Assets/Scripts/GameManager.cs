db160fae0b08e7643d0a97ba7d108cc8958c41f7
tinzie <benjamin.mitchell@live.co.uk>
2021-02-20 14:28:10 +0000

    jumpin n duckin m8

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : Singleton<GameManager>
 {
-	public float moveSpeed = 2;
+	public float moveSpeed = 4;
 
+	//0 = happy land, 1 = scary land
+	private int currentWorld = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +18,15 @@ public class GameManager : Singleton<GameManager>
     // Update is called once per frame
     void Update()
     {
-        
-    }
+		if (Input.GetKeyDown(KeyCode.G))
+		{
+
+		}
+
+	}
+
+	public void EndGame()
+	{
+		SceneManager.LoadScene("Main");
+	}
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	public float jumpVelocity;
+
+	private Rigidbody2D rgdBody2D;
+	private BoxCollider2D boxCollider2D;
+
+	private bool canJump = true;
+	private bool sliding = false;
+	private float colliderHeight, playerHeight;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		boxCollider2D = GetComponent<BoxCollider2D>();
+		colliderHeight = boxCollider2D.size.y;
+		playerHeight = transform.localScale.y;
+
+		rgdBody2D = GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space) && canJump)
+		{
+			rgdBody2D.velocity = Vector2.up * jumpVelocity;
+			canJump = false;
+		}
+
+		if (Input.GetKeyDown(KeyCode.X) && !sliding)
+		{
+			StartCoroutine(Slide());
+			sliding = false;
+		}
+	}
+
+	private IEnumerator Slide()
+	{
+		sliding = true;
+		//boxCollider2D.size = new Vector2( boxCollider2D.size.x, colliderHeight / 2.0f);
+		//boxCollider2D.offset = new Vector2(boxCollider2D.offset.x, boxCollider2D.offset.y - colliderHeight / 2.0f);
+		transform.localScale = new Vector2(transform.localScale.x, playerHeight / 2.0f);
+
+		yield return new WaitForSeconds(0.5f);
+
+		sliding = false;
+		//boxCollider2D.size = new Vector2(boxCollider2D.size.x, colliderHeight);
+		//boxCollider2D.offset = new Vector2(boxCollider2D.offset.x, boxCollider2D.offset.y + colliderHeight / 2.0f);
+		transform.localScale = new Vector2(transform.localScale.x, playerHeight);
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if(collision.gameObject.CompareTag("Floor"))
+		{
+			canJump = true;
+		}
+
+		if(collision.gameObject.CompareTag("Lethal"))
+		{
+			GameManager.Instance.EndGame();
+		}
+	}
+}
