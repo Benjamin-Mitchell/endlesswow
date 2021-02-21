57f6e76a0f522e0d5786d14c9f9bf70b0582ddb2
tinzie <benjamin.mitchell@live.co.uk>
2021-02-21 11:33:14 +0000

    Animooshuns

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,9 +11,10 @@ public class Player : MonoBehaviour
 	private Rigidbody2D rgdBody2D;
 	private BoxCollider2D boxCollider2D;
 	private SpriteRenderer spriteRenderer;
+	private Animator animator;
 
 	private bool canJump = true;
-	private bool sliding = false;
+	private bool grounded = false;
 	private float colliderHeight, playerHeight;
 	private float gameMoveSpeed;
 
@@ -31,6 +32,7 @@ public class Player : MonoBehaviour
 
 		rgdBody2D = GetComponent<Rigidbody2D>();
 		spriteRenderer = GetComponent<SpriteRenderer>();
+		animator = GetComponent<Animator>();
 
 		gameMoveSpeed = GameManager.Instance.moveSpeed;
 		layerMask = LayerMask.GetMask("Floor");
@@ -39,6 +41,9 @@ public class Player : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
+		animator.SetFloat("VertVelocity", rgdBody2D.velocity.y);
+		animator.SetFloat("HorizVelocity", rgdBody2D.velocity.x);
+		Debug.Log(animator.GetFloat("VertVelocity"));
 		Vector3 rotationTargetVector = Vector3.right;
 
 		if (currentWorld == GameManager.CURRENT_WORLD.HAPPY_LAND)
@@ -52,16 +57,16 @@ public class Player : MonoBehaviour
 
 			if (Input.GetKeyDown(KeyCode.X) /*&& !sliding*/)
 			{
-				sliding = true;
-				transform.localScale = new Vector2(transform.localScale.x, playerHeight / 2.0f);
+				animator.SetTrigger("StartSlide");
+				//transform.localScale = new Vector2(transform.localScale.x, playerHeight / 2.0f);
 				//StartCoroutine(Slide());
 				//sliding = false;
 			}
 
 			if (Input.GetKeyUp(KeyCode.X))
 			{
-				sliding = false;
-				transform.localScale = new Vector2(transform.localScale.x, playerHeight);
+				animator.SetTrigger("StopSlide");
+				//transform.localScale = new Vector2(transform.localScale.x, playerHeight);
 			}
 		}
 		else if (currentWorld == GameManager.CURRENT_WORLD.SCARY_LAND)
@@ -79,23 +84,35 @@ public class Player : MonoBehaviour
 				rgdBody2D.velocity = new Vector2(rgdBody2D.velocity.x, -moveVelocity);
 			}
 
-			//Always start moving towards center of the screen
-			if (transform.position.x < 1.5f)
-				Physics2D.gravity = new Vector2(Mathf.Abs(transform.position.x), 0.0f);
-			else
-				Physics2D.gravity = new Vector2(0.0f, 0.0f);
+			grounded = false;
 			
 			//raycast for moving floors. Stops the character from getting stuck in them.
-			RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right, 1.0f, layerMask);
+			RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right, 0.9f, layerMask);
 			if (hit.collider != null)
 			{
 				if (hit.collider.gameObject.CompareTag("Floor"))
-				{	
+				{
+					grounded = true;
 					//do something to stay up....
 					float moveDelta = gameMoveSpeed * Time.deltaTime;
 					transform.position = new Vector3(transform.position.x - moveDelta, transform.position.y, transform.position.z);
 				}
 			}
+
+			animator.SetBool("Grounded", grounded);
+
+			//Always start moving towards center of the screen
+			float stopPos = -2.0f;
+			if (transform.position.x < stopPos && !grounded)
+			{
+				rgdBody2D.velocity = new Vector2(Mathf.Abs(transform.position.x + stopPos), rgdBody2D.velocity.y);
+			}
+			//Physics2D.gravity = new Vector2(Mathf.Abs(transform.position.x + stopPos), 0.0f);
+			//else
+			//{
+			//	Physics2D.gravity = new Vector2(0.0f, 0.0f);
+			//	rgdBody2D.velocity = new Vector2(0.0f, rgdBody2D.velocity.y);
+			//}
 		}
 
 		//rotation for falling
@@ -108,22 +125,23 @@ public class Player : MonoBehaviour
 	public void SetWorld(GameManager.CURRENT_WORLD world)
 	{
 		currentWorld = world;
+		animator.SetBool("BadLand", world == GameManager.CURRENT_WORLD.SCARY_LAND ? true : false);
 	}
 
-	private IEnumerator Slide()
-	{
-		sliding = true;
-		//boxCollider2D.size = new Vector2( boxCollider2D.size.x, colliderHeight / 2.0f);
-		//boxCollider2D.offset = new Vector2(boxCollider2D.offset.x, boxCollider2D.offset.y - colliderHeight / 2.0f);
-		transform.localScale = new Vector2(transform.localScale.x, playerHeight / 2.0f);
-
-		yield return new WaitForSeconds(0.50f);
-
-		sliding = false;
-		//boxCollider2D.size = new Vector2(boxCollider2D.size.x, colliderHeight);
-		//boxCollider2D.offset = new Vector2(boxCollider2D.offset.x, boxCollider2D.offset.y + colliderHeight / 2.0f);
-		transform.localScale = new Vector2(transform.localScale.x, playerHeight);
-	}
+	//private IEnumerator Slide()
+	//{
+	//	sliding = true;
+	//	//boxCollider2D.size = new Vector2( boxCollider2D.size.x, colliderHeight / 2.0f);
+	//	//boxCollider2D.offset = new Vector2(boxCollider2D.offset.x, boxCollider2D.offset.y - colliderHeight / 2.0f);
+	//	//transform.localScale = new Vector2(transform.localScale.x, playerHeight / 2.0f);
+	//
+	//	yield return new WaitForSeconds(0.50f);
+	//
+	//	sliding = false;
+	//	//boxCollider2D.size = new Vector2(boxCollider2D.size.x, colliderHeight);
+	//	//boxCollider2D.offset = new Vector2(boxCollider2D.offset.x, boxCollider2D.offset.y + colliderHeight / 2.0f);
+	//	//transform.localScale = new Vector2(transform.localScale.x, playerHeight);
+	//}
 
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
